6ab1eba4c5c11278c6f8ed09c17318bc74a2ae28
taylan <tylnercan94@gmail.com>
2022-05-02 23:58:01 +0300

    Created Bashekim model which extends User, implemented constructor that gets arguments from Super class. Removed connector-J 3.0.0 and added 3.0.4 release to project's referenced libraries instead because 3.0.0 is not compatable with JDK 15 compiler.

---
+++ javaProject/src/Model/Bashekim.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Bashekim extends User {
+
+	public Bashekim(int id, String tc_No, String password, String name, String type) {
+		super(id, tc_No, password, name, type);
+		
+	}
+	
+	public Bashekim() {}
+}
