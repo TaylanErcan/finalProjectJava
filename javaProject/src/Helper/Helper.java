7eeafe1e25fff61412f4ee831041ce610b278b00
taylan <tylnercan94@gmail.com>
2022-05-04 19:11:56 +0300

    About Helper class
    Added confirmation method for deleting operation(Are you sure to delete
    this user?)
    Modified OptionPane button text values by creating a new method(using
    UIManager in it) in Helper class. yes no cancel ok button

--- javaProject/src/Helper/Helper.java
+++ javaProject/src/Helper/Helper.java
@@ -1,11 +1,21 @@
 package Helper;
 
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 public class Helper {
-
+	
+	public static void optionPaneChangeYesNoButtonText() {
+		UIManager.put("OptionPane.cancelButtonText", "Ýptal");
+		UIManager.put("OptionPane.noButtonText", "Hayýr");
+		UIManager.put("OptionPane.okButtonText", "Tamam");
+		UIManager.put("OptionPane.yesButtonText", "Evet");
+	}
+	
+	
 	public static void showMessage(String str) {
 		String msg;
+		optionPaneChangeYesNoButtonText();
 		switch(str) {
 		case "fill":
 			msg= "Lütfen tüm alanlarý doldurunuz.";
@@ -20,4 +30,26 @@ public class Helper {
 		
 		JOptionPane.showMessageDialog(null,msg,"Hata",JOptionPane.INFORMATION_MESSAGE);
 	}
+	
+	public static boolean confirm(String str) {
+		String message;
+		optionPaneChangeYesNoButtonText();
+		switch(str){
+		case "sure":
+			message="Bu iþlemi gerçekten yapmak istiyor musunuz?";
+			break;
+		default:
+			message=str;
+			break;
+		}
+		
+		int result= JOptionPane.showConfirmDialog(null, message, "Dikkat!",JOptionPane.YES_NO_OPTION);	
+		if(result==0) { // yes
+			return true;
+		}else { // no
+			return false;
+		}
+	}
+	
+	
 }
