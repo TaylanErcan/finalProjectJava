257af709f6af606073aa18c9b4387a03e02de88e
taylan <tylnercan94@gmail.com>
2022-05-11 21:14:42 +0300

    Login and DoctorGUI implemantations
    added button and components that are required for selecting date and
    time.
    wrote the necessary codes that retreives date info from interface and
    performs insert operation for working hour.
    updated LoginGUI to handle doctor-type login process.

--- javaProject/src/View/DoctorGUI.java
+++ javaProject/src/View/DoctorGUI.java
@@ -15,13 +15,23 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTabbedPane;
 import com.toedter.calendar.JDateChooser;
-import javax.swing.JComboBox;
 
+import Helper.Helper;
+
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.awt.event.ActionEvent;
 
 public class DoctorGUI extends JFrame {
 
 	private JPanel doctorReservationPane;
 	private static Doctor doctor = new Doctor();
+	private JTable tableWHour;
 
 	/**
 	 * Launch the application.
@@ -62,22 +72,65 @@ public class DoctorGUI extends JFrame {
 		lblDoctorWelcome.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 16));
 		lblDoctorWelcome.setBounds(10, 11, 225, 22);
 		doctorReservationPane.add(lblDoctorWelcome);
-		
+
 		JTabbedPane workingTabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		workingTabbedPane.setBounds(22, 90, 538, 310);
 		doctorReservationPane.add(workingTabbedPane);
-		
+
 		JPanel workingHoursPanel = new JPanel();
 		workingHoursPanel.setBackground(Color.WHITE);
 		workingTabbedPane.addTab("Çalýþma Saatleri", null, workingHoursPanel, null);
 		workingHoursPanel.setLayout(null);
-		
-		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(10, 11, 102, 26);
-		workingHoursPanel.add(dateChooser);
-		
+
+		JDateChooser doctorSelectDate = new JDateChooser();
+		doctorSelectDate.setBounds(10, 11, 125, 26);
+		workingHoursPanel.add(doctorSelectDate);
+
 		JComboBox cbxSelectTime = new JComboBox();
-		cbxSelectTime.setBounds(145, 11, 79, 26);
+		cbxSelectTime.setFont(new Font("SansSerif", Font.PLAIN, 13));
+		cbxSelectTime.setModel(new DefaultComboBoxModel(
+				new String[] { "10:00", "11:00", "11:30", "12:30", "14:00", "14:45", "15:40" }));
+		cbxSelectTime.setBounds(145, 11, 70, 26);
 		workingHoursPanel.add(cbxSelectTime);
+
+		JButton btnAddWHour = new JButton("Ekle");
+		btnAddWHour.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				String date = "";
+				try {
+					date = sdf.format(doctorSelectDate.getDate());
+				} catch (Exception e2) {
+
+				}
+				if (date.length() == 0) {
+					Helper.showMessage("Lütfen geçerli bir tarih seçiniz.");
+				} else {
+					String time = " " + cbxSelectTime.getSelectedItem().toString() + ":00";
+					String selectedDateTime = date + time;
+					try {
+						boolean control = doctor.addWorkHour(doctor.getId(), doctor.getName(), selectedDateTime);
+						if (control) {
+							Helper.showMessage("success");
+						} else {
+							Helper.showMessage("error");
+						}
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}
+
+			}
+		});
+		btnAddWHour.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		btnAddWHour.setBounds(419, 11, 104, 26);
+		workingHoursPanel.add(btnAddWHour);
+
+		JScrollPane scrollPaneWHour = new JScrollPane();
+		scrollPaneWHour.setBounds(10, 48, 513, 223);
+		workingHoursPanel.add(scrollPaneWHour);
+
+		tableWHour = new JTable();
+		scrollPaneWHour.setViewportView(tableWHour);
 	}
 }
--- javaProject/src/View/LoginGUI.java
+++ javaProject/src/View/LoginGUI.java
@@ -25,6 +25,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.JPasswordField;
 import Helper.*;
 import Model.Bashekim;
+import Model.Doctor;
 
 public class LoginGUI extends JFrame {
 
@@ -33,7 +34,7 @@ public class LoginGUI extends JFrame {
 	private JTextField txtDoktorTc;
 	private JPasswordField passwordFieldDoktor;
 	private JPasswordField passwordFieldHasta;
-	private DBConnection conn= new DBConnection();
+	private DBConnection conn = new DBConnection();
 
 	/**
 	 * Launch the application.
@@ -63,107 +64,121 @@ public class LoginGUI extends JFrame {
 		wrapper_pane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(wrapper_pane);
 		wrapper_pane.setLayout(null);
-		
+
 		JLabel lbl_logo = new JLabel(new ImageIcon(getClass().getResource("hospitalIcon72x72.png")));
 		lbl_logo.setBounds(205, 11, 116, 81);
 		wrapper_pane.add(lbl_logo);
-		
+
 		JLabel lblNewLabel = new JLabel("Hastane Y\u00F6netim Sistemine Ho\u015Fgeldiniz");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Georgia", Font.ITALIC, 18));
 		lblNewLabel.setBounds(89, 93, 349, 28);
 		wrapper_pane.add(lblNewLabel);
-		
+
 		JTabbedPane wrapper_TabPane = new JTabbedPane(JTabbedPane.TOP);
 		wrapper_TabPane.setBounds(37, 146, 458, 238);
 		wrapper_pane.add(wrapper_TabPane);
-		
+
 		JPanel panelDoctor = new JPanel();
 		panelDoctor.setBackground(Color.WHITE);
 		wrapper_TabPane.addTab("Doktor Giriþi", null, panelDoctor, null);
 		panelDoctor.setLayout(null);
-		
+
 		JLabel lblDoktorTc = new JLabel("T.C Numaran\u0131z:");
 		lblDoktorTc.setBounds(50, 16, 130, 28);
 		lblDoktorTc.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
 		panelDoctor.add(lblDoktorTc);
-		
+
 		JLabel lblDoktorPassword = new JLabel("\u015Eifre:");
 		lblDoktorPassword.setBounds(77, 55, 55, 28);
 		lblDoktorPassword.setFont(new Font("Segoe UI Semibold", Font.ITALIC, 16));
 		panelDoctor.add(lblDoktorPassword);
-		
+
 		txtDoktorTc = new JTextField();
 		txtDoktorTc.setBounds(203, 18, 162, 28);
 		txtDoktorTc.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		txtDoktorTc.setColumns(10);
 		panelDoctor.add(txtDoktorTc);
-		
+
 		JButton btnDoktorLogin = new JButton("Giri\u015F Yap");
-		
+
 		btnDoktorLogin.setBounds(50, 138, 341, 43);
 		panelDoctor.add(btnDoktorLogin);
-		
+
 		passwordFieldDoktor = new JPasswordField();
 		passwordFieldDoktor.setBounds(203, 62, 162, 20);
 		panelDoctor.add(passwordFieldDoktor);
-		
+
 		JPanel panelHasta = new JPanel();
 		panelHasta.setBackground(Color.WHITE);
 		wrapper_TabPane.addTab("Hasta Giriþi", null, panelHasta, null);
 		panelHasta.setLayout(null);
-		
+
 		JLabel lblHastaTc = new JLabel("T.C Numaran\u0131z:");
 		lblHastaTc.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
 		lblHastaTc.setBounds(50, 16, 130, 28);
 		panelHasta.add(lblHastaTc);
-		
+
 		JLabel lblHastaSifre = new JLabel("\u015Eifre:");
 		lblHastaSifre.setFont(new Font("Segoe UI Semibold", Font.ITALIC, 16));
 		lblHastaSifre.setBounds(79, 55, 55, 28);
 		panelHasta.add(lblHastaSifre);
-		
+
 		txtHastaTc = new JTextField();
 		txtHastaTc.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		txtHastaTc.setText("fsdfkshhfsf");
 		txtHastaTc.setBounds(203, 18, 162, 28);
 		panelHasta.add(txtHastaTc);
 		txtHastaTc.setColumns(10);
-		
+
 		JButton btnHastaRegister = new JButton("Kay\u0131t Ol");
 		btnHastaRegister.setBounds(60, 138, 142, 43);
 		panelHasta.add(btnHastaRegister);
-		
+
 		JButton btnHastaLogin = new JButton("Giri\u015F Yap");
 		btnHastaLogin.setBounds(249, 138, 142, 43);
 		panelHasta.add(btnHastaLogin);
-		
+
 		passwordFieldHasta = new JPasswordField();
 		passwordFieldHasta.setBounds(203, 62, 162, 20);
 		panelHasta.add(passwordFieldHasta);
-		
+
 		btnDoktorLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(txtDoktorTc.getText().length() == 0 || passwordFieldDoktor.getText().length() == 0) {
+				if (txtDoktorTc.getText().length() == 0 || passwordFieldDoktor.getText().length() == 0) {
 					Helper.showMessage("fill");
-				}else {
+				} else {
 					try {
-						Connection con= conn.conDb();
-						Statement st= con.createStatement();
-						ResultSet resultSet= st.executeQuery("SELECT * FROM user");
-						while(resultSet.next()) {
-							if(txtDoktorTc.getText().equals(resultSet.getString("Tc_No")) &&
-									passwordFieldDoktor.getText().equals(resultSet.getString("Password"))) {
-								Bashekim bhekim= new Bashekim();
-								bhekim.setId(resultSet.getInt("Id"));
-								bhekim.setTc_No(resultSet.getString("Tc_No"));
-								bhekim.setPassword(resultSet.getString("Password"));
-								bhekim.setName(resultSet.getString("Name"));
-								bhekim.setType(resultSet.getString("Type"));
-								BashekimGUI bGUI= new BashekimGUI(bhekim);
-								bGUI.setVisible(true);
-								dispose();
-								
+						Connection con = conn.conDb();
+						Statement st = con.createStatement();
+						ResultSet resultSet = st.executeQuery("SELECT * FROM user");
+						while (resultSet.next()) {
+							if (txtDoktorTc.getText().equals(resultSet.getString("Tc_No"))
+									&& passwordFieldDoktor.getText().equals(resultSet.getString("Password"))) {
+								if (resultSet.getString("Type").equals("Bashekim")) {
+									Bashekim bhekim = new Bashekim();
+									bhekim.setId(resultSet.getInt("Id"));
+									bhekim.setTc_No(resultSet.getString("Tc_No"));
+									bhekim.setPassword(resultSet.getString("Password"));
+									bhekim.setName(resultSet.getString("Name"));
+									bhekim.setType(resultSet.getString("Type"));
+									BashekimGUI bGUI = new BashekimGUI(bhekim);
+									bGUI.setVisible(true);
+									dispose();
+								}
+
+								if (resultSet.getString("Type").equals("Doktor")) {
+									Doctor doctor = new Doctor();
+									doctor.setId(resultSet.getInt("Id"));
+									doctor.setTc_No(resultSet.getString("Tc_No"));
+									doctor.setPassword(resultSet.getString("Password"));
+									doctor.setName(resultSet.getString("Name"));
+									doctor.setType(resultSet.getString("Type"));
+									DoctorGUI dGUI = new DoctorGUI(doctor);
+									dGUI.setVisible(true);
+									dispose();
+								}
+
 							}
 						}
 					} catch (SQLException ex) {
